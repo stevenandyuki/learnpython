9202c9c0acf2d3276443df940a0bdca8e5dbdd89
unknown <244071358@qq.com>
2018-04-20 09:25:38 +0800

    q

--- learn package/learnprogram/recursive function.py
+++ learn package/learnprogram/recursive function.py
@@ -1,6 +1,12 @@
-def fun(n):
-    if n==1:
-        return 1
-    return n*fun(n-1)
+# -*- coding: utf-8 -*-
 
-print fun(100)
+def move(n,A,B,C):
+    if n==1:
+        print A,"->",C
+    else :
+        move(n-1,A,C,B)
+        move(1,A,B,C)
+        move(n-1,B,A,C)
+
+move(4,'A','B','C')
+
